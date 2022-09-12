fdfa31b4068236757f9b0c67fe25c98184408be6
Salmandotweb <salmandotweb@gmail.com>
2022-09-12 05:50:14 -0700

    updated

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+	user: "postgres",
+	password: "salman@4200",
+	host: "localhost",
+	port: 5432,
+	database: "perntodo",
+});
+
+module.exports = pool;
